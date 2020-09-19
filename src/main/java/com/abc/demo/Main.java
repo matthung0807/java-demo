b7c84135d2b9d4dd4560d15e2ff61ca9695da67a
matthung0807 <bronx0807@gmail.com>
2020-09-19 20:01:10 +0800

    modify

--- src/main/java/com/abc/demo/Main.java
+++ src/main/java/com/abc/demo/Main.java
@@ -5,13 +5,12 @@ import lombok.SneakyThrows;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 public class Main {
 
-//    @SneakyThrows(IOException.class)
     public static void main(String[] arges) {
-
+        String path = System.getProperty("user.home") + "/Documents/tmp/test.txt";
+        new Main().writeFile("hello world", path);
     }
 
     void writeFile(String str, String path) {
@@ -26,21 +25,5 @@ public class Main {
             fos.write(data);
         }
     }
-
-//    void write(byte[] data, String path) {
-//        File file = new File(path);
-//        try (FileOutputStream fos = new FileOutputStream(file)) {
-//            fos.write(data); // 寫出檔案
-//        } catch (IOException e) {
-//            // 例外處理
-//        }
-//    }
-
-//    void write(byte[] data, String path) throws IOException {
-//        File file = new File(path);
-//        try (FileOutputStream fos = new FileOutputStream(file)) {
-//            fos.write(data); // 寫出檔案
-//        }
-//    }
 
 }
