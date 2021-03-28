45de1ae686c7fef6a9078207ed562d09a5074095
matthung0807 <bronx0807@gmail.com>
2021-03-28 21:51:24 +0800

    RabbitMQ java client producer

--- src/main/java/com/abc/demo/Producer.java
+++ src/main/java/com/abc/demo/Producer.java
@@ -17,8 +17,9 @@ public class Producer {
                 Connection connection = factory.newConnection();
                 Channel channel = connection.createChannel()
         ) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             String message = "Hello World!";
+            
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
             System.out.println(" [x] Sent '" + message + "'");
         } catch (Exception e) {
