3692b03d4c67f4e6985881dacd5ecf355e160f3c
matthung0807 <bronx0807@gmail.com>
2021-12-25 20:24:59 +0800

    get data from kinesis data streams

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # java-demo
-[Java 從本機發送資料到Kinesis Data Streams](https://matthung0807.blogspot.com/2021/12/java-localhost-put-data-to-kinesis-data-streams.html.l)
+[Java 本機讀取Kinesis Data Streams的資料](https://matthung0807.blogspot.com/2021/12/java-localhost-get-data-from-kinesis-data-streams.html)
--- src/main/java/com/abc/demo/Main.java
+++ src/main/java/com/abc/demo/Main.java
@@ -1,12 +1,11 @@
 package com.abc.demo;
 
-import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.kinesis.KinesisClient;
-import software.amazon.awssdk.services.kinesis.model.PutRecordRequest;
-import software.amazon.awssdk.services.kinesis.model.PutRecordResponse;
+import software.amazon.awssdk.services.kinesis.model.*;
 
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -14,17 +13,39 @@ public class Main {
         KinesisClient kinesisClient = KinesisClient.builder()
                 .region(Region.AP_NORTHEAST_1)
                 .build();
+        getDataFromKinesisDataStream(kinesisClient, "KinesisDataStreamDemo");
+        kinesisClient.close();
+    }
 
-        String data = "Hello world";
-
-        PutRecordRequest putRecordRequest = PutRecordRequest.builder()
-                .streamName("KinesisDataStreamDemo")
-                .partitionKey("demo-001")
-                .data(SdkBytes.fromString(data, StandardCharsets.UTF_8))
+    public static void getDataFromKinesisDataStream(KinesisClient kinesisClient, String streamName) {
+        DescribeStreamRequest describeStreamRequest = DescribeStreamRequest.builder()
+                .streamName(streamName)
                 .build();
 
-        PutRecordResponse putRecordResponse = kinesisClient.putRecord(putRecordRequest);
-        String sequenceNumber = putRecordResponse.sequenceNumber();
-        System.out.println(sequenceNumber);
+        DescribeStreamResponse streamResponse = kinesisClient.describeStream(describeStreamRequest);
+        List<Shard> shards = new ArrayList<>(streamResponse.streamDescription().shards());
+        String lastShardId = shards.get(shards.size() - 1).shardId();
+
+        GetShardIteratorRequest itReq = GetShardIteratorRequest.builder()
+                .streamName(streamName)
+                .shardIteratorType(ShardIteratorType.TRIM_HORIZON)
+                .shardId(lastShardId)
+                .build();
+
+        GetShardIteratorResponse shardIteratorResult = kinesisClient.getShardIterator(itReq);
+        String shardIterator = shardIteratorResult.shardIterator();
+
+        GetRecordsRequest recordsRequest = GetRecordsRequest.builder()
+                .shardIterator(shardIterator)
+                .limit(1000) // Set maximum records to return to 1000.
+                .build();
+
+        GetRecordsResponse result = kinesisClient.getRecords(recordsRequest);
+
+        List<Record> records = result.records();
+        for (Record record : records) {
+            System.out.println(new String(record.data().asByteArray()));
+        }
     }
+
 }
