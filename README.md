6745bbfbded164c86a4d5eb75f7e620df62e35fe
matthung0807 <bronx0807@gmail.com>
2022-03-29 23:08:51 +0800

    switch expression

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # java-demo
-Java Demo
+[Java Switch expression用法](https://matthung0807.blogspot.com/2022/03/java-switch-expression.html)
--- src/main/java/com/abc/demo/Main.java
+++ src/main/java/com/abc/demo/Main.java
@@ -3,7 +3,24 @@ package com.abc.demo;
 public class Main {
 
     public static void main(String[] arges) {
-        System.out.println("hello world");
+        var color = Color.WHITE;
+
+        var value = switch (color) {
+            case RED -> 1;
+            case BLUE -> 2;
+            case BLACK, WHITE -> {
+                if((int) (Math.random() * 3) > 1) {
+                    yield 0;
+                }
+                yield 1;
+            }
+            default -> -1;
+        };
+        System.out.println(value);
     }
 
+}
+
+enum Color {
+    RED, BLUE, GREEN, WHITE, BLACK,
 }
