80d111ed77702229cf13f954f117e9b2fe4594cb
matthung0807 <bronx0807@gmail.com>
2022-04-01 22:46:19 +0800

    localhost receive message from SQS queue

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # java-demo
-Java Demo
+[Java 本機接收SQS queue的訊息](https://matthung0807.blogspot.com/2022/04/java-localhost-receive-message-from-sqs-queue.html)
--- src/main/java/com/abc/demo/Main.java
+++ src/main/java/com/abc/demo/Main.java
@@ -1,9 +1,49 @@
 package com.abc.demo;
 
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.*;
+
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] arges) {
-        System.out.println("hello world");
+        SqsClient sqsClient = SqsClient.builder()
+                .region(Region.AP_NORTHEAST_1)
+                .build();
+        String queueUrl = getQueueUrl(sqsClient);
+        List<Message> messageList = getMessageListFromSQS(sqsClient, queueUrl);
+        System.out.println(messageList.size());
+        if (!messageList.isEmpty()) {
+            var message = messageList.get(0);
+            System.out.println(message.messageId());
+            System.out.println(message.body());
+        }
+
+    }
+
+    public static String getQueueUrl(SqsClient sqsClient) {
+        GetQueueUrlResponse getQueueUrlResponse =
+                sqsClient.getQueueUrl(
+                        GetQueueUrlRequest.builder()
+                                .queueName("DemoQueue")
+                                .build());
+        return getQueueUrlResponse.queueUrl();
+    }
+
+    public static List<Message> getMessageListFromSQS(SqsClient sqsClient, String queueUrl) {
+        try {
+            ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+                    .queueUrl(queueUrl)
+                    .maxNumberOfMessages(5)
+                    .build();
+            return sqsClient.receiveMessage(receiveMessageRequest).messages();
+        } catch (SqsException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
+        return Collections.emptyList();
     }
 
 }
